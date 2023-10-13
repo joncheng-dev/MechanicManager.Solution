d7c9a7b5e903120d16cbefad470ea70721386f98
Jonathan Cheng <jcheng461@gmail.com>
2023-10-13 11:25:27 -0700

    WIP: models & database schema; sketching in Miro

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -2,5 +2,10 @@ using System.Collections.Generic;
 
 namespace MechanicManager.Models
 {
-
+  public class Machine
+  {
+    public int MachineId { get; set; }
+    public string ModelName { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+  }
 }
--- Factory/Models/MechanicManagerContext.cs
+++ Factory/Models/MechanicManagerContext.cs
@@ -4,6 +4,10 @@ namespace MechanicManager.Models
 {
   public class MechanicManagerContext : DbContext
   {
+    public DbSet<Engineer> Engineers { get; set; }
+    public DbSet<Machine> Machines { get; set; }
+    public DbSet<EngineerMachine> EngineerMachines { get; set; }
+
     public MechanicManagerContext(DbContextOptions options) : base(options) { }
   }
 }
