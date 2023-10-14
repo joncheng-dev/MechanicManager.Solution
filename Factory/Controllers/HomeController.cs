9af2f58e0b903394ca5ebe1b2b657d3ed3fdc534
Jonathan Cheng <jcheng461@gmail.com>
2023-10-13 17:00:10 -0700

    WIP: display all engineers and machines in splash (home) page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using MechanicManager.Models;
 
 namespace MechanicManager.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly MechanicManagerContext _db;
+
+    public HomeController(MechanicManagerContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
       ViewBag.PageTitle = "Mechanic Manager";
-      return View();
+      Engineer[] engineersArray = _db.Engineers.ToArray();
+      Machine[] machinesArray = _db.Machines.ToArray();
+      Dictionary<object[], object[]> model = new Dictionary<object[], object[]>();
+      model.Add("engineers", engineersArray);
+      model.Add("machines", machinesArray);
+      return View(model);
     }
   }
 }
