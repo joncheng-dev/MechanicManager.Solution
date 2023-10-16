3d418885036c57229ef458bf4acdb57c382865b6
Jonathan Cheng <jcheng461@gmail.com>
2023-10-15 20:37:00 -0700

    update regex string to allow certain characters in database annotation

---
+++ Factory/Migrations/20231016033342_UpdateRegexToAllowSpaceInNameString.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MechanicManager.Migrations
+{
+    public partial class UpdateRegexToAllowSpaceInNameString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,7 +6,7 @@ namespace MechanicManager.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
-    [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Please enter up to 40 alphanumeric characters. '_' is also a valid character.")]
+    [RegularExpression(@"^[a-zA-Z0-9_. ]+$", ErrorMessage = "Please enter up to 40 alphanumeric characters. '_' is also a valid character.")]
     [Required(ErrorMessage = "Please enter a valid name for the engineer.")]
     public string Name { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
